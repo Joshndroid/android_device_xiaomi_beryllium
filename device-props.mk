2ae1c889c616091ff2f1621714535f7347f2e3af
TheScarastic <warabhishek@gmail.com>
2018-11-02 16:17:49 -0400

    beryllium: Remove common display post-processing and dead camera props
     * Used ones were moved to the common tree and the rest to the recycle bin.
    
    Change-Id: Ifc73e5bb73fab83fa6a46eb5fdd8748066e58255

--- device-props.mk
+++ device-props.mk
@@ -4,10 +4,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.qcom.ad=1 \
-    ro.qcom.ad.calib.data=/vendor/etc/sdr_config.cfg \
-    ro.qcom.ad.hdr.calib.data=/vendor/etc/hdr_config.cfg \
-    ro.qcom.ad.sensortype=2 \
-    ro.qualcomm.cabl=0 \
     ro.sf.lcd_density=400
 
